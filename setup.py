eabdade81240363b59a785b2ff846f4c49bdf80e
Jake Mor <jakemny@gmail.com>
2018-01-12 13:45:29 -0500

    done

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import shutil, os, fnmatch
+import shutil, os, fnmatch, sys
 
 print """
 
@@ -9,31 +9,32 @@ print """
 	      /|                            /| 
 	     (/                            (/  
 
+
 	https://github.com/jakemor/SparkPlug
 
-	NOTE: If you fuck up, answering the questions,
+	If you fuck up while answering the questions,
 	delete this folder and git clone again.
 
 	----------------------------- 
 	B E F O R E   S T A R T I N G  
 	----------------------------- 
 
-1. 	Create a new project in Google Cloud Platform], 
-	then set up App Engine within that project. Take 
-	note of the `Project id`. 
+1.  Create a new project in Google Cloud Platform], 
+    then set up App Engine within that project. Take 
+    note of the `Project id`. 
 
-2. 	Create a Cloud SQL Instance within the project as
-	well. Take note of the `Instance id` and the 
-	`Root password` that you choose during setup. When
-	setup is completed, take note of the `Instance
-	connection name` of your new instance. 
+2.  Create a Cloud SQL Instance within the project as
+    well. Take note of the `Instance id` and the 
+    `Root password` that you choose during setup. When
+    setup is completed, take note of the `Instance
+    connection name` of your new instance. 
 
-3. 	When you see that setup is complete, click on the
-	instance id, and go to Databases. Create a new
-	database and name it the name of your project.
+3.  When you see that setup is complete, click on the
+    instance id, and go to Databases. Create a new
+    database and name it the name of your project.
 
-4. 	Next go to Authorization and add your home network
-	so you can connect to the database from home. 
+4.  Next go to Authorization and add your home network
+    so you can connect to the database from home. 
 
 """
 
@@ -65,6 +66,8 @@ questions = [
 	("SQL_INSTANCE_CONNECTION_NAME","What is the 'Instance connection name' of your sql instance?")
 ]
 
+answers = []
+
 # rename folders and variables
 for q in questions:
 	keyName = q[0]
@@ -72,18 +75,32 @@ for q in questions:
 
 	keyValue = raw_input(question + " ")
 
-	findReplace(PROJECT_DIRECTORY, keyName, keyValue, "*.py")
-	findReplace(PROJECT_DIRECTORY, keyName, keyValue, "*.yaml")
+	answers.append((keyName, keyValue))
+
+answer = ""
+
+while not (answer.lower() == "yes" or answer.lower() == "y" or answer.lower() == "n" or answer.lower() == "no"):
+	answer = raw_input("Are all the above fields correct? (Y/n) ")
+
+if (answer.lower() == "n" or answer.lower() == "no"):
+	print """
+	Alrighty then, aborting.
+	"""
+	sys.exit(0)
+
+for a in answers:
+	findReplace(PROJECT_DIRECTORY, a[0], a[1], "*.py")
+	findReplace(PROJECT_DIRECTORY, a[0], a[1], "*.yaml")
 
 
 # delete readme
-os.remove('README.md')
+# os.remove('README.md')
 
 # delete .git
-shutil.rmtree('.git')
+# shutil.rmtree('.git')
 
 # delete setup.py
-os.remove('setup.py')
+# os.remove('setup.py')
 
 print """
 Complete. 
